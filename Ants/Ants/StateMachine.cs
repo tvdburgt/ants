39e486e24c168bcd9450d2d742346e952d02bdec
Tijmen van der Burgt <tvdburgt@gmail.com>
2012-06-03 15:14:15 +0200

    added fsm

---
+++ Ants/Ants/StateMachine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ants.States;
+using Microsoft.Xna.Framework;
+
+namespace Ants
+{
+    public class StateMachine
+    {
+        private State currentState;
+
+        public StateMachine()
+        {
+        }
+
+        public StateMachine(State state)
+        {
+            ChangeState(state);
+        }
+
+        public void Update(GameTime time)
+        {
+            if (currentState != null)
+                currentState.Update(time);
+        }
+
+        public void ChangeState(State state)
+        {
+            if (currentState != null)
+                currentState.Exit();
+
+            currentState = state;
+            currentState.Enter();
+        }
+    }
+}
---
+++ Ants/Ants/States/State.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Ants.Model;
+
+namespace Ants.States
+{
+    public abstract class State
+    {
+        protected Ant Agent { get; private set; }
+        protected AntsGame Game { get; private set; }
+
+        public State(AntsGame game, Ant agent)
+        {
+            Agent = agent;
+            Game = game;
+        }
+
+        public abstract void Update(GameTime time);
+
+        public virtual void Enter()
+        {
+            Console.WriteLine("Entered state {0} for entity {1}", GetType(), Agent.GetType());
+        }
+
+        public virtual void Exit()
+        {
+            Console.WriteLine("Exited state {0} for entity {1}", GetType(), Agent.GetType());
+        }
+    }
+}
